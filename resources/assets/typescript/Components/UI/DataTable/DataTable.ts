ec666b13b044f1d342ba7e798acdb23fba0e9175
Vladimir Chemeris <chamav@gmail.com>
2016-06-20 00:06:40 +0500

    migrate template to views

--- resources/assets/typescript/Components/UI/DataTable/DataTable.ts
+++ resources/assets/typescript/Components/UI/DataTable/DataTable.ts
@@ -3,45 +3,7 @@ import {DataTableDirectives} from 'angular2-datatable/datatable';
 
 @Component({
     selector: 'data-table',
-    template: `<table class="table table-striped" [mfData]="data" #mf="mfDataTable" [mfRowsOnPage]="10">
-                <thead>
-                <tr>
-                    <th style="width: 10%"></th>
-                    <th style="width: 20%">
-                        <mfDefaultSorter by="name">Name</mfDefaultSorter>
-                    </th>
-                    <th style="width: 40%">
-                        <mfDefaultSorter by="email">Email</mfDefaultSorter>
-                    </th>
-                    <th style="width: 10%">
-                        <mfDefaultSorter by="age">Age</mfDefaultSorter>
-                    </th>
-                    <th style="width: 20%">
-                        <mfDefaultSorter by="city">City</mfDefaultSorter>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr *ngFor="#item of mf.data">
-                    <td>
-                      <button class="btn btn-danger" title="remove" (click)="removeItem(item)">
-                        <span class="glyphicon glyphicon-remove"></span>
-                      </button>
-                    </td>
-                    <td>{{item.name}}</td>
-                    <td>{{item.email}}</td>
-                    <td class="text-right">{{item.age}}</td>
-                    <td>{{item.city | uppercase}}</td>
-                </tr>
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td colspan="5">
-                        <mfBootstrapPaginator [rowsOnPageSet]="[5,10,15]"></mfBootstrapPaginator>
-                    </td>
-                </tr>
-                </tfoot>
-            </table>`
+    'templateUrl': '/templates/UIComponents.DataTable.main',
 })
 export class DataTableDirectives {
     private data: any[] = [
